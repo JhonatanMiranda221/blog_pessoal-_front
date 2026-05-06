5b5708bd5caddab5cfa286334530b394e354e119
JhonatanMiranda221 <jhonatanmiranda221@gmail.com>
2026-05-06 13:45:23 -0300

    React

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+    return (
+<>
+<div className ="flex justify-center bg-amber-300">
+    <h1 className='font-bold text-3xl'>Teste</h1>
+</div>
+</>
+    )
+}
+ 
+export default Home
