d8c7c880810f3ec879454f2f30471ced9f5dbe4c
JhonatanMiranda221 <jhonatanmiranda221@gmail.com>
2026-05-14 11:17:34 -0300

    feat: implementa estilização e lógica do formulário de postagem

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,33 +1,40 @@
-import React from 'react'
+import ListaPostagens from '../../components/postagem/listapostagens/Listapostagens';
+import ModalPostagem from '../../components/postagem/modalpostagem/ModalPostagem';
 
 function Home() {
     return (
         <>
-            <div className='bg-indigo-900 flex justify-center' >
-                <div className='container grid-cols-2 text-white'>
-                    <div className='flex flex-col gap-4 items-center justify-center py-4'>
-                        <h2 className='text-5xl font-bold'>Seja bem Vinde!</h2>
-                        <p className='text-xl'>
+            <div className="bg-indigo-900 flex justify-center">
+              
+                <div className="container grid grid-cols-2 text-white">
+                    
+                   
+                    <div className="flex flex-col gap-4 items-center justify-center py-4">
+                        <h2 className="text-5xl font-bold">Seja bem Vinde!</h2>
+                        <p className="text-xl">
                             Expresse aqui seus pensamentos e opiniões
-                            </p>
-                        <div className='flex justify-center gap-4'>
-                            <div className='rounded text-white border-white border-solid border-2 py-2 px-4'>
-                                Nova Postagem
-                            </div>
+                        </p>
+                        <div className="flex justify-center gap-4">
+                            <ModalPostagem />
                         </div>
                     </div>
-
-                    <div className='flex justify-center'>
+                
+                    <div className="flex justify-center">
                         <img 
-                        src="https://i.imgur.com/fyfri1v.png"
-                        alt="Imagem Pagina Home"
-                        className='w-2/3'
+                            src="https://i.imgur.com/fyfri1v.png"
+                            alt="Imagem Pagina Home"
+                            className="w-2/3"
                         />
                     </div>
-                </div>
-            </div>
+                    
+
+                </div> 
+            </div> 
+       
+            
+            <ListaPostagens />
         </>
-    )
+    );
 }
  
-export default Home
+export default Home;
